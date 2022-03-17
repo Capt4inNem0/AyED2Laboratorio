149e2fc2beaa6e1dab7a849a60288a25cfc2c6ef
Usuario Visita <visitante@lab07.famaf.unc.edu.ar>
2022-03-17 17:11:56 -0300

    Cambiado a kickstart hecho por profesores

--- lab0/max_min.c
+++ lab0/max_min.c
@@ -1,47 +1,41 @@
-#include <stdio.h>
-#include <assert.h>
+#include <stdio.h>    /* printf(), scanf() */
+#include <stdbool.h>  /* tipo bool         */
 
+#include <assert.h>   /* assert() */
+
+#define ARRAY_SIZE 10
 
 struct max_min_result {
-	int max_value;
-	int min_value;
-	unsigned int max_position;
-	unsigned int min_position;
+    int max_value;
+    int min_value;
+    unsigned int max_position;
+    unsigned int min_position;
+};
+
+struct max_min_result compute_max_min(int array[], unsigned int length)
+{
+    assert(length > 0);
+    array = array;
+    struct max_min_result result = { 0, 0, 0, 0 };
+    result.max_value = 2;
+    result.min_value = 2;
+
+    // IMPLEMENTAR
+
+    return result;
 }
 
-struct max_min_result compute_max_min(int array[], unsigned int length){
-	struct max_min_result result;
-	result.max_value = array[0];
-	result.min_value = array[0];
-	int i = 0;
-	int currentValue;
-	assert(length > 0);
-	while(i < length){
-		currentValue = array[i];
-		if(currentValue > result.max_value){
-			result.max_value = currentValue;
-			result.max_position = i;
-		}
-		if(currentValue < result.min_value){ 
-			result.min_value = currentValue;
-			result.min_position = i;
-		}
-		i = i+1;
-	}
-	return result;
-}
+int main(void)
+{
+    int array[ARRAY_SIZE] = { 4, -1, 5, 8, 9, 0, 3, 6, 0, 0 };
 
-int main(void){
-	runTest();
-	
-	return 0;
-}
+    // PEDIR AL USUARIO QUE INGRESE LOS ELEMENTOS DEL ARREGLO.
 
-int runTest(){
-	struct max_min_result expected = compute_max_min({1,2,3,-5,6,7,80,79}, 8);
-	assert(expected.min_value == -5);
-	assert(expected.min_position == 3);
-	assert(expected.max_value == 80);
-	assert(expected.max_position == 6);
+    struct max_min_result result = compute_max_min(array, ARRAY_SIZE);
+    printf("Máximo: %d\n", result.max_value);
+    printf("Posición del máximo: %u\n", result.max_position);
+    printf("Mínimo: %d\n", result.min_value);
+    printf("Posición del mínimo: %u\n", result.min_position);
+    return 0;
 }
 
